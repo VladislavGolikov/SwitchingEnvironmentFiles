fd69761b0a0a190db024c29e5174279b99333b0e
Vladislav Golikov <vladislav.v.golikov@gmail.com>
2021-11-24 13:59:17 +0300

    раскрасил строки сообщений о работе в postinstall.js

--- postinstall.js
+++ postinstall.js
@@ -1,16 +1,17 @@
 ﻿const path=require('path');
 const fs=require('fs');
+const colors=require('colors');
 
 const fileName1=path.resolve(__dirname, `my_change_mode.cmd`);
 const fileName2=path.join(__dirname,`..`,`.bin`, `my_change_mode.cmd`);
 
 try{
     fs.copyFileSync(fileName1, fileName2);
-    console.log('Для запуска скрипта использовать команду: "npm run sw"');
+    console.log('Для запуска скрипта использовать команду: "npm run sw"'.brightGreen);
 }
 catch(err){
     console.log(err);
-    console.log('Не удалось скопировать скрипт для запуска');
+    console.log('Не удалось скопировать скрипт для запуска'.red);
 }
 
 
