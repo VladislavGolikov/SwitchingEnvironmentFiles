459463ef6022679327843e3909aba818fec2fbf4
Vladislav Golikov <vladislav.v.golikov@gmail.com>
2021-11-30 16:57:24 +0300

    изменен сценарий запуска и установки пакета: удален преанинсталл, и командник для запуска, постинсталл теперь должен менять package.json целевого проекта для быстрого запуска

--- my_change_mode.cmd
+++
@@ -1,5 +0,0 @@
-
-
-rem start node ../mychange/my_change_mode.js - старый путь!
-
-start node ./node_modules/mychange/my_change_mode.js
--- postinstall.js
+++ postinstall.js
@@ -2,19 +2,29 @@
 const fs=require('fs');
 const colors=require('colors');
 
-const fileName1=path.resolve(__dirname, `my_change_mode.cmd`);
-const fileName2=path.join(__dirname,`..`,`.bin`, `my_change_mode.cmd`);
+const fileName=path.resolve(__dirname, `package.json`);
+
+const command=`start node ./node_modules/mychange/my_change_mode.js`;
+const key=`sw`;
+const scriptName=`mychange`;
+//const fileName2=path.join(__dirname,`..`,`.bin`, `my_change_mode.cmd`);
 
 try{
-    fs.copyFileSync(fileName1, fileName2);
-    console.log('Для запуска скрипта использовать команду: "npm run sw"'.brightGreen);
-    console.log('\n\nТакже для запуска скрипта использовать команду: "npx my_change_mode"'.brightGreen);
+    let jsonFromFile=JSON.parse(fs.readFileSync(fileName));
 
+
+    jsonFromFile.scripts.sw=command;
+
+    console.log(`В ${fileName} был добавлен ключ ${key.bgGreen} для запуска пакета ${scriptName.bgGreen}`.yellow);
+
+    fs.writeFileSync(fileName,JSON.stringify(jsonFromFile,null,`  `))
+
+    console.log(`\n\n Для запуска скрипта ${scriptName.bgGreen} использовать команду: `.yellow+`npm run sw`.green);
 
 }
 catch(err){
     console.log(err);
-    console.log('Не удалось скопировать скрипт для запуска'.red);
+    console.log('Не удалось добавить ключ для запуска'.red);
 }
 
 
