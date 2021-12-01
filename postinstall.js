b78f523bdd5318bc5d5586a32210dafe45ef1571
Vladislav Golikov <vladislav.v.golikov@gmail.com>
2021-12-01 12:34:19 +0300

    очередное улучшение вида для редми.мд

--- postinstall.js
+++ postinstall.js
@@ -2,22 +2,20 @@
 const fs=require('fs');
 const colors=require('colors');
 
-//const fileName=path.resolve(__dirname, `package.json`);
-
 const command=`start node ./node_modules/mychange/my_change_mode.js`;
 const key=`sw`;
 const scriptName=`mychange`;
 const fileName=path.join(__dirname,`..`,`..`, `package.json`);
 
 try{
-    let jsonFromFile=JSON.parse(fs.readFileSync(fileName));
+    const jsonFromFile=JSON.parse(fs.readFileSync(fileName));
 
 
     jsonFromFile.scripts.sw=command;
 
-    console.log(`В ${fileName} был добавлен ключ ${key.bgGreen} для запуска пакета ${scriptName.bgGreen}`.yellow);
-
     fs.writeFileSync(fileName,JSON.stringify(jsonFromFile,null,`  `))
+
+    console.log(`В ${fileName} был добавлен ключ ${key.bgGreen} для запуска пакета ${scriptName.bgGreen}`.yellow);
 
     console.log(`\n\n Для запуска скрипта ${scriptName.bgGreen} использовать команду: `.yellow+`npm run sw`.green);
 
@@ -26,8 +24,6 @@ catch(err){
     console.log(err);
     console.log('Не удалось добавить ключ для запуска'.red);
 }
-
-
 
 /* задержка закрытия консоли */
 setTimeout(()=>console.log(''),5000);
